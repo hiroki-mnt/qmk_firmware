e15931a0b5af57f91efb4f617725a40fc1ada136
mametech <mametech.notes@gmail.com>
2025-03-21 18:28:50 +0900

    feat: A長押しALTの追加

--- keyboards/keychron/k2_max/ansi/rgb/keymaps/via_custom/keymap.c
+++ keyboards/keychron/k2_max/ansi/rgb/keymaps/via_custom/keymap.c
@@ -22,7 +22,12 @@ enum layers {
     MAC_FN,
     WIN_BASE,
     WIN_FN,
-    WIN_CUSTOM1  
+    WIN_CUSTOM1,
+    WIN_A_TAP_DANCE,
+};
+
+enum {
+    TD_A_LAYER = 0,  // タップダンスのキー定義
 };
 
 // clang-format off
@@ -47,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_DEL,   RGB_MOD,
      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
+     KC_CAPS,  TD(TD_A_LAYER),     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
      KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,    KC_END,
      KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT, MO(WIN_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
@@ -58,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
      _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,            _______,  _______,  _______,
      _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______),
-     
+
 [WIN_CUSTOM1] = LAYOUT_ansi_84(
      _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  RGB_TOG,
      _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
@@ -68,10 +73,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______)
 };
 
+void alt_layer_tap(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1 && !state->pressed) {
+        // 短押し → Aを入力
+        tap_code(KC_A);
+    } else if (state->pressed) {
+        // 長押し → Layer WIN_A_TAP_DANCEを有効化しつつAltを押しっぱなし
+        layer_on(WIN_A_TAP_DANCE);
+        register_code(KC_LALT);
+    }
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_A_LAYER] = ACTION_TAP_DANCE_FN(alt_layer_tap)
+};
+
+
 // clang-format on
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_record_keychron_common(keycode, record)) {
         return false;
+    }
+
+    // TD_A_LAYER の処理を追加
+    switch (keycode) {
+        case TD(TD_A_LAYER):
+            if (!record->event.pressed) {
+                layer_off(WIN_A_TAP_DANCE); // レイヤーを解除
+                unregister_code(KC_LALT);  // Altキーを離す
+            }
+            return false;  // デフォルトの処理を無効化
     }
     return true;
 }
